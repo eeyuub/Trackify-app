088679001de3efe834190ee5b11cc0631e8ff582
eeyuub <ayoubfcb2002@gmail.com>
2025-01-12 21:46:58 +0100

    enable Cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,21 +7,25 @@ import { ProductModule } from './product-module/product.module';
 import { OrderModule } from './order-module/order.module';
 import { ConfigModule } from '@nestjs/config';
 import { QrcodeGeneratorModule } from './qrcode-generator-module/qrcode-generator.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'order-app',
-      synchronize: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
     }),
     OrderAppModule,
     ProductModule,
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow cookies and credentials
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+  });
+  
   await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
